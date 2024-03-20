5e343a34af6fdee6f906c21f7a88a4c7599eba7b
Elena Peris <elena.peris@trucksters.com>
2024-03-20 19:29:23 +0100

    remove clg

--- src/services/users.ts
+++ src/services/users.ts
@@ -16,7 +16,6 @@ export const getUsers = async () => {
     const response = await fetch(`https://jsonplaceholder.typicode.com/users/`)
     const json = await response.json()
 
-      console.log(json)
     return json?.map((user: User) => ({
         id: user.id,
         name: user.name,
